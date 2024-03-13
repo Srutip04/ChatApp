e8ee648fe7b95f15c03d8d048ef8d86fb172b34f
Srutip04 <subuhishruti75@gmail.com>
2024-03-13 10:36:04 +0530

    Add MongoDB connection and user model

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import connectToMongoDB from "./db/connect.js";
 
 const app = express();
 
@@ -9,8 +10,11 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello WOrld");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello WOrld");
+// });
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`server running on port ${PORT}`);
+});
