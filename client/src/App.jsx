b70d30be07f69e7b9f800d9787e814056b75395c
Srutip04 <subuhishruti75@gmail.com>
2024-03-14 01:43:56 +0530

    setting up routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,32 @@
-
-import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-
   return (
     <>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+      <div className="p-4 h-screen flex items-center justify-center">
+        <Routes>
+          <Route
+            path="/"
+            element={ <Home />}
+          />
+          <Route
+            path="/login"
+            element={ <Login />}
+          />
+          <Route
+            path="/signup"
+            element={ <Signup />}
+          />
+        </Routes>
+        <Toaster />
+      </div>
     </>
   );
 }
 
-export default App
+export default App;
